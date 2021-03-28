d2272ef2f1ff7b33856524e5f4a7e4eaf9f2f705
Federico De Vietro <fede.devietro@gmail.com>
2021-03-28 19:59:42 +0200

    Pew added new commands

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ async def on_ready():
 async def on_ready():
   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
 
-  
-  
 
 #ask stuff
 answers = ('The answer was inside of you all along','Ask someone else','Mayhaps...','Almost certainly','The cheese holds your answer','insert clever answer here','Yes, quite','Why would you care?','My sources say yes','Absolutely not','Of course','Uhhh... yeah, sure','Hard to tell','Obama knows')
@@ -30,13 +28,37 @@ async def howdumb(ctx, arg):
   percent = random.randint(0,100)
   await ctx.send('**'+arg+' is '+str(percent)+'%'+' dumb'+'**')
 
-#roll 2 dices
+#howsimp
+@bot.command(name='howsimp')
+async def howsimp(ctx, arg):
+  percent = random.randint(0,100)
+  await ctx.send('**'+arg+' is '+str(percent)+'%'+' simp'+'**')
+
+#howmonke
+@bot.command(name='howmonke')
+async def howmonke(ctx, arg):
+  percent = random.randint(0,100)
+  await ctx.send('**'+arg+' is '+str(percent)+'%'+' monke'+'**')
+
+#roll dice
 @bot.command(name='roll')
 async def roll(ctx):
+  rollres = random.randint(1,6)
+  await ctx.send('**'+str(rollres)+'**')
+
+#roll 2 dices
+@bot.command(name='roll2')
+async def roll2(ctx):
   rollres = random.randint(1,6)
   rollres1 = random.randint(1,6)
   rolltot = rollres + rollres1
   await ctx.send('**'+str(rollres)+' + '+str(rollres1)+' = '+str(rolltot)+'**')
+
+#cointoss
+@bot.command(name='cointoss')
+async def cointoss(ctx):
+ cointossGame = ['head', 'tail'] 
+ await ctx.send('**''Result:'{random.choice(cointossGame)}'**')
 
 #rock paper scissors
 @bot.command(name='rps')
@@ -47,10 +69,20 @@ async def rps(ctx, user_choice):
   elif user_choice.lower() not in rpsGame:
     await ctx.send('**Error** This command only works with rock, paper, or scissors.')
 
+#conspirecaesar
+@bot.command(name='conspirecaesar')
+async def conspire(ctx):
+ await ctx.send('TWENTY-FOUR STAB WOUNDS,YOU DID NOT WANT TO LEAVE HIM A CHANCE,HUH?')
+
 #conspire
 @bot.command(name='conspire')
 async def conspire(ctx, userchoice):
-
+  conspirators = random.randint(0,30)
+  stabwounds = random.randint(1,60)
+  if conspirators > 10 and stabwounds >5:
+    await ctx.send('**'f'You manage to start a conspire against: `{user_choice}` with `{conspirators}` people, you manage to kill`{user_choice}` with `{stabwounds}`stabwounds**')
+  else:
+    await ctx.send('**'f'You try to start a conspire against: `{user_choice}` but you fail. **`')
 
 token = ""
 bot.run(token, bot = True)
