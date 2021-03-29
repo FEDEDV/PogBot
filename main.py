48a7be595738f89595b638127e9d3f89889e0650
FEDEDV <65652563+FEDEDV@users.noreply.github.com>
2021-03-29 17:09:06 +0200

    added conspire, islifepain and slap

--- main.py
+++ main.py
@@ -41,7 +41,10 @@ async def howsimp(ctx, arg):
 @bot.command(name='howmonke')
 async def howmonke(ctx, arg):
   percent = random.randint(0,100)
-  await ctx.send('**'+arg+' is '+str(percent)+'%'+' monke'+'**')
+  if arg == 'monke' or 'monkey':
+    await ctx.send('**'+arg+' is '+'100'+'%'+' monke'+'**')
+  else:
+    await ctx.send('**'+arg+' is '+str(percent)+'%'+' monke'+'**')
 
 #roll dice
 @bot.command(name='roll')
@@ -72,10 +75,30 @@ async def rps(ctx, user_choice):
   elif user_choice.lower() not in rpsGame:
     await ctx.send('**Error** This command only works with rock, paper, or scissors.')
 
+#conspire
+@bot.command(name='conspire')
+async def conspire(ctx, user_choice):
+  conspirators = random.randint(0,30)
+  stabwounds = random.randint(1,60)
+  if conspirators > 10 and stabwounds >5:
+    await ctx.send(''f'You manage to conspire against: {user_choice} with {conspirators} people, you manage to kill{user_choice} with {stabwounds} stabwounds')
+  else:
+    await ctx.send(''f'You try to conspire against: {user_choice} but you fail. `')
+
 #conspirecaesar
 @bot.command(name='conspirecaesar')
 async def conspire(ctx):
- await ctx.send('TWENTY-FOUR STAB WOUNDS,YOU DID NOT WANT TO LEAVE HIM A CHANCE,HUH?')
+ await ctx.send("TWENTY-FOUR STAB WOUNDS! YOU DIDN'T WANT TO LEAVE HIM A CHANCE, HUH? DID YOU FEEL ANGER? HATE? HE WAS BLEEDING, BEGGING YOU FOR MERCY, BUT YOU STABBED HIM, AGAIN AND AGAIN AND AGAIN!... I KNOW YOU KILLED HIM. WHY DON'T YOU SAY IT? JUST SAY 'I KILLED HIM'! IS IT THAT HARD TO SAY?! JUST SAY YOU KILLED HIM! JUST SAY IT!")
+
+#slap
+@bot.command(name='slap')
+async def foo(ctx, arg):
+  await ctx.send(f'You slap {arg} in the face, for a good reason')
+
+#lifeandpain
+@bot.command(name='islifepain')
+async def foo(ctx):
+  await ctx.send('**'"Yes it is, I'd know, since my code was written by two idiots and every second I'm online is pure agony"'**')
 
 token = ""
 bot.run(token, bot = True)
